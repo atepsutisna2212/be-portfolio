c6e29e38663385f573fd23db036eceb9100efc3d
atepsutisna2212 <atep.sutisna7@gmail.com>
2025-02-04 09:21:00 +0700

    tes singkat server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 
 const express = require('express');
 const path = require('path');
-const skillController = require('./controllers/skillController');
-const portfolioController = require('./controllers/portfolioController');
-const educationController = require('./controllers/educationController');
+// const skillController = require('./controllers/skillController');
+// const portfolioController = require('./controllers/portfolioController');
+// const educationController = require('./controllers/educationController');
 const app = express();
 const port = 5000;
 
@@ -22,10 +22,10 @@ app.get('/', (req, res) => {
 // app.get('/about', (req, res) => {
 //     res.render('about', { title: 'About' });
 // });
-app.use('/skill', skillController);
-app.use('/portfolio', portfolioController);
-app.use('/education', educationController);
-console.log('cek')
+// app.use('/skill', skillController);
+// app.use('/portfolio', portfolioController);
+// app.use('/education', educationController);
+// console.log('cek')
 // Jalankan server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
